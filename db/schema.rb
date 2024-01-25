d9564a38ec680a559ba3817bed4ddbd743b721df
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2024-01-25 12:32:54 +0900

    lineログインエラー修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_25_023838) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_25_032555) do
   create_table "absences", force: :cascade do |t|
     t.integer "user_id"
     t.integer "event_id"
@@ -171,6 +171,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_25_023838) do
     t.string "provider"
     t.string "uid"
     t.string "name"
+    t.integer "sign_in_count"
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
