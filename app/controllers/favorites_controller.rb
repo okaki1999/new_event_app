5a68e38325bf26d7ca7ab88c1c65bb05ba67158e
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2024-01-29 12:18:36 +0900

    Lineの通知制限

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,10 +2,6 @@ class FavoritesController < ApplicationController
   def create
     @favorite = current_user.favorites.create(event_id: params[:event_id])
     @event = Event.find(params[:event_id])
-    user_id = ENV['LINE_USER_ID'] # ユーザーのIDに実際のIDを設定
-    event_link = url_for(@event)
-    message = "#{@favorite.user.username}さんが#{@event.start_time.strftime("%m月/%d日")}のイベント「#{@event.title}」に参加する予定です\nまだ未定の方も早めの参加、不参加、保留の投票をお願いします!\nURLは\n#{event_link}"
-    LineService.broadcast(message)
     session[:previous_url] = request.referer
     redirect_back(fallback_location: root_path)
   end
